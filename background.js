b322d8c44cb8dd7987e004209826f7db542f465d
Stacy <sslearn07@gmail.com>
2021-02-11 03:15:01 -0500

    changes

--- background.js
+++ background.js
@@ -1,15 +1,41 @@
-let lng, lat;
+console.log('hey')
+// // get zipcode from user input
+// document.addEventListener(
+//   'DOMContentLoaded',
+//   function () {
+//     const checkPageButton = document.getElementById('submitZipcode');
+//     checkPageButton.addEventListener(
+//       'click',
+//       function () {
+//         // variable to store zipcode
+//         const x = document.getElementById('zipcodeInput').value;
+        
+//         chrome.tabs.getSelected(null, 
+//           // insert function to be executed
+//         );
+//       },
+//       false
+//     );
+//   },
+//   false
+// );
 
-let timeZURL = `http://api.timezonedb.com/v2.1/get-time-zone?key=68AJZPSMRSDC&format=json&by=position&lat=${lat}&lng=${lng}`
 
-let sunSRURL = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
 
-fetch(timeZURL)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
 
-fetch(sunURL)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+// let lng, lat;
+
+// let timeZURL = `http://api.timezonedb.com/v2.1/get-time-zone?key=68AJZPSMRSDC&format=json&by=position&lat=${lat}&lng=${lng}`
+
+// let sunSRURL = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
+
+// fetch(timeZURL)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
+// fetch(sunURL)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+// a
--- popup.js
+++ popup.js
@@ -1,16 +1,43 @@
-// Stand reminder: remind to stand up every 50 minutes past the hour
-// import {latLng} from './geo.js';
+// timer
+// if the timer button is pressed, convert input time:
+// HH * 3600000 ms, MM * 60000 ms, 60 ms 
+// set alert to display after
+
+
+/*
 document.addEventListener(
   'DOMContentLoaded',
   function () {
-    const checkPageButton = document.getElementById('clickIt');
+    const checkPageButton = document.getElementById('timer');
+    checkPageButton.addEventListener(
+      'click',
+      function () {
+        const interval = document.getElementById('userInput').value;
+        
+        chrome.tabs.getSelected(null, 
+          // invoke timesUpAlert after intervalConversion
+          setTimeout(timesUpAlert, intervalConversion)
+        );
+      },
+      false
+    );
+  },
+  false
+);
+*/
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    const checkPageButton = document.getElementById('timer');
     checkPageButton.addEventListener(
       'click',
       function () {
         const interval = document.getElementById('userInput').value;
         const intervalConversion = interval * 1000;
         chrome.tabs.getSelected(null, 
-          setTimeout(standAlert, intervalConversion)
+          // invoke timesUpAlert after intervalConversion
+          setTimeout(timesUpAlert, intervalConversion)
         );
       },
       false
@@ -19,13 +46,123 @@ document.addEventListener(
   false
 );
 
-// console.log(latLng[55795]);
 
-function standAlert() {
+// document.addEventListener(
+//   'DOMContentLoaded',
+//   function () {
+//     const interval = document.getElementById('userInput').value;
+//     const display = document.querySelector('#time')
+//     const checkPageButton = document.getElementById('timer');
+//     checkPageButton.addEventListener(
+//       'click',
+
+//       function startTimer(duration = interval * 5, display) {
+//         var timer = duration, minutes, seconds;
+//         setInterval(function () {
+//             minutes = parseInt(timer / 60, 10)
+//             seconds = parseInt(timer % 60, 10);
+      
+//             minutes = minutes < 10 ? "0" + minutes : minutes;
+//             seconds = seconds < 10 ? "0" + seconds : seconds;
+      
+//             display.textContent = minutes + ":" + seconds;
+      
+//             if (--timer < 0) {
+//                 timer = duration;
+//             }
+//         }, 1000);
+//       }
+      
+//       window.onload = function () {
+//         var fiveMinutes = 60 * 5,
+//             display = document.querySelector('#time');
+//         startTimer(fiveMinutes, display);
+//       };
+
+
+//       /*
+//       function () {
+//         const interval = document.getElementById('userInput').value;
+        
+//         chrome.tabs.getSelected(null, 
+//           // invoke timesUpAlert after intervalConversion
+//           setTimeout(timesUpAlert, intervalConversion)
+//         */
+
+//         );
+//       },
+//       false
+//     );
+//   },
+//   false
+// );
+
+
+
+
+
+
+function timesUpAlert() {
   // const interval = document.getElementById('userInput').value;
   // const intervalConversion = interval * 1000;
-  alert('hi')
+  alert('hello, goodbye!')
+  // timer: take user input (HH/MM/SS)
+  
+  // alarm:
+
+
 }
+
+// // alarm
+// document.addEventListener(
+//   'DOMContentLoaded',
+//   function () {
+//     const checkPageButton = document.getElementById('alarm');
+//     checkPageButton.addEventListener(
+//       'click',
+//       function () {
+//         const inputTime = document.getElementById('timerInput').value;
+//         const today = new Date();
+//         const currentHour = Number(today.getHours())
+//         const currentMin = Number(today.getMinutes())
+//         // const currentSec = Number(today.getSeconds())
+//         // current time in ms
+//         const currentTime = (currentHour * 3600000) + (currentMin * 60000);
+//         // console.log('typeofCurr', typeof currentTime, currentTime)
+//         // create variable timeConversion
+//         // timeConversion will equal inputTime minus current time converted to miliseconds
+//         // hours: inputHour - currentHour * ms conversion ...
+//         // console.log('TIMEE', inputTime)
+//         let hours = Number(`${inputTime[0]}` + `${inputTime[1]}`)
+//         let minutes = Number(`${inputTime[3]}` + `${inputTime[4]}`)
+//         // let seconds = Number(`${inputTime[6]}` + `${inputTime[7]}`)
+//         // input time in ms
+//         const inputTimeToMs = (hours * 3600000) + (minutes * 60000);
+//         // const inputTimeToMs = (hours * 3600000) + (minutes * 60000) + (seconds * 60);
+//         // console.log(hours, minutes, seconds, 'sec', `${inputTime[6]}`, `${inputTime[7]}`)
+//         // console.log('typeofInput', typeof inputTimeToMs, inputTimeToMs)
+//         // hours = hours - currentHour;
+//         // console.log('new hr', hours);
+//         // minutes = minutes - currentMin;
+//         // seconds = seconds - currentSec;
+//         // (console.log('typeof', typeof hours, typeof minutes, typeof seconds))
+//         // (console.log(hours, minutes, seconds))
+//         const timeConversion = inputTimeToMs - currentTime;
+//         // console.log('a', typeof timeConversion, timeConversion)
+//         chrome.tabs.getSelected(null, function(tab) {
+          
+//         }
+//           // invoke timesUpAlert after intervalConversion
+//           // setTimeout(timesUpAlert, timeConversion)
+//         );
+//       },
+//       false
+//     );
+//   },
+//   false
+// );
+
+
 
 
 
@@ -41,3 +178,120 @@ function (tab) {
   // setTimeout(alert('Get up and move'), intervalConversion);
 }
 */
+
+
+
+
+/*
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    const checkPageButton = document.getElementById('clickIt');
+    checkPageButton.addEventListener(
+      'click',
+      function () {
+        const interval = document.getElementById('userInput').value;
+        const intervalConversion = interval * 1000;
+        chrome.tabs.getSelected(null, 
+          // invoke timesUpAlert after intervalConversion
+          setTimeout(timesUpAlert, intervalConversion)
+        );
+      },
+      false
+    );
+  },
+  false
+);
+
+
+/*
+// console.log(latLng[55795]);
+function 
+function timesUpAlert() {
+  // const interval = document.getElementById('userInput').value;
+  // const intervalConversion = interval * 1000;
+  alert('hi')
+  // timer: take user input (HH/MM/SS)
+  
+  // alarm:
+
+
+}
+
+*/
+
+
+
+
+
+// document.addEventListener(
+//   'DOMContentLoaded',
+//   function () {
+//     const checkPageButton = document.getElementById('timer');
+//     checkPageButton.addEventListener(
+//       'click',
+//       function () {
+//         const inputTime = document.getElementById('timerInput').value;
+//         // console.log('time', inputTime)
+//         // get the hours in inputTime
+//         let hours = Number(`${inputTime[0]}` + `${inputTime[1]}`)
+//         // console.log('hour', hours, 'typeof', typeof hours)
+//         // get the minutes in inputTime
+//         let minutes = Number(`${inputTime[3]}` + `${inputTime[4]}`)
+//         // get the seconds in inputTime
+//         let seconds = Number(`${inputTime[6]}` + `${inputTime[7]}`)
+//         console.log('sec', seconds)
+//         if (!hours) hours = 0;
+//         if (!minutes) minutes = 0;
+//         // if (!seconds) seconds = 0;
+
+//         console.log('no min', hours, minutes, seconds);
+//         // convert time to ms
+//         const timeConversion = (hours * 3600000) + (minutes * 60000) + (seconds * 60);
+//         console.log('a', timeConversion)
+//         chrome.tabs.getSelected(null, 
+//           // invoke timesUpAlert after intervalConversion
+//           setTimeout(timesUpAlert, timeConversion)
+//         );
+//       },
+//       false
+//     );
+//   },
+//   false
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function startTimer(duration, display) {
+//   var timer = duration, minutes, seconds;
+//   setInterval(function () {
+//       minutes = parseInt(timer / 60, 10)
+//       seconds = parseInt(timer % 60, 10);
+
+//       minutes = minutes < 10 ? "0" + minutes : minutes;
+//       seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//       display.textContent = minutes + ":" + seconds;
+
+//       if (--timer < 0) {
+//           timer = duration;
+//       }
+//   }, 1000);
+// }
+
+// window.onload = function () {
+//   var fiveMinutes = 60 * 5,
+//       display = document.querySelector('#time');
+//   startTimer(fiveMinutes, display);
+// };
